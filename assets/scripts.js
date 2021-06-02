791638d0edce453284f6162cfd356297c43f844b
shannongailhoo <61385913+shannongailhoo@users.noreply.github.com>
2021-06-03 03:50:40 +0800

    Add files via upload
    donut chart

--- assets/scripts.js
+++ assets/scripts.js
@@ -24,26 +24,26 @@ $(document).ready(function() {
 	});
 	
 	
-	var areas = new Array();
+	var selectedAreas = ["africa","antarctica","asia","australia","europe","north","south"];
 	$('.area').click(function() {
 		if($(this).is(':checked'))
-        	areas.push($(this).attr("id"));
+			selectedAreas.push($(this).attr("id"));
         else {
-			var index = areas.indexOf($(this).attr("id"));
-			areas.splice(index,1);
+			var index = selectedAreas.indexOf($(this).attr("id"));
+			selectedAreas.splice(index,1);
 		}
-		console.log(areas);
+		console.log(selectedAreas);
 	});
 
-	var fuels = new Array();
+	var selectedFuels = ['coal','gas','hydro','nuclear','oil','solar','waste','wind','others'];
 	$('.fuel').click(function() {
 		if($(this).is(':checked'))
-			fuels.push($(this).attr("id"));
+		selectedFuels.push($(this).attr("id"));
         else {
-			var index = fuels.indexOf($(this).attr("id"));
-			fuels.splice(index,1);
+			var index = selectedFuels.indexOf($(this).attr("id"));
+			selectedFuels.splice(index,1);
 		}
-		console.log(fuels);
+		console.log(selectedFuels);
 	});
 	
 	d3.csv("Environment_Temperature_change_E_All_Data_NOFLAG.csv", function(data1) {
@@ -51,4 +51,51 @@ $(document).ready(function() {
 			console.log(data1);
 		});
 	});
+
+	
+// DONUT
+	var allDonutFuels = ['coal','gas','hydro','nuclear','oil','solar','waste','wind','others'];
+		let darknude = '#6B5E5B';
+		let rose = '#a3664a';
+		let mustard = '#d6b85a';
+		let orange = '#F0A35E';
+		let purple = '#663a82';
+		let greenish = '#70ae98';
+		let teal = '#055b56';
+		let coralblue = '#657fa6';
+		let deepblue = '#02273a';
+		let darkgray = '#808080';
+	var allDonutColors = [darknude,rose,mustard,purple,greenish,teal,coralblue,deepblue,darkgray];
+	// var allDonutColors = ['red','orange','yellow','green','teal','blue','violet','pink','gray'];
+	// var selectedFuels = ['coal','gas','hydro','nuclear'];
+	// var selectedColors = ["red", "blue", "yellow", "green"];
+	var selectedColors = [];
+	var data = [];
+	var sum = 0;
+	// var data = [10,80,20,30,50,20,10,5,20];
+
+	for(let i=0;i<selectedFuels.length;i++) {
+		selectedColors.push(allDonutColors[allDonutFuels.indexOf(selectedFuels[i])]);
+		sum += data[i];
+	// data.push(10*i+10);
+	}
+
+	var r = 120;
+	var color = d3.scale.ordinal().range(selectedColors);
+	var canvas = d3.select("#donut-chart").append("svg").attr("width", 325).attr("height", 250);
+	var group = canvas.append("g").attr("transform", "translate(120,120)");
+	var arc = d3.svg.arc().innerRadius(50).outerRadius(r);
+	var pie = d3.layout.pie().value(function (d, i) { return data[i]; });
+	var arcs = group.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc");
+	arcs.append("path").attr("d", arc).attr("fill", function(d,i) {return selectedColors[i];});
+	arcs.append("text").attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";})
+		.attr("text-anchor", "middle").attr("font-size", "12px").style("fill", "white")
+		.text(function(d,i) {return data[i] / sum + "%";});
+
+	var legendG = canvas.selectAll(".legend").data(pie(data)).enter().append("g")
+				.attr("transform", function(d,i){ return "translate(" + (260) + "," + (i * 15 + 20) + ")"; })
+				.attr("class", "legend");
+	legendG.append("circle").attr("r", 5).attr("fill", function(d,i) { return selectedColors[i]; });
+	legendG.append("text").text(function(d,i) {return selectedFuels[i];}).style("font-size", 12).attr("y", 3).attr("x", 11);
+
 });
