3a829f9a702a9f5949100777c0ee97e75c1592df
shannongailhoo <61385913+shannongailhoo@users.noreply.github.com>
2021-06-03 04:21:50 +0800

    added hover in donut

--- assets/scripts.js
+++ assets/scripts.js
@@ -70,9 +70,9 @@ $(document).ready(function() {
 	// var selectedFuels = ['coal','gas','hydro','nuclear'];
 	// var selectedColors = ["red", "blue", "yellow", "green"];
 	var selectedColors = [];
-	var data = [];
+	// var data = [];
 	var sum = 0;
-	// var data = [10,80,20,30,50,20,10,5,20];
+	var data = [10,80,20,30,50,20,10,5,20];
 
 	for(let i=0;i<selectedFuels.length;i++) {
 		selectedColors.push(allDonutColors[allDonutFuels.indexOf(selectedFuels[i])]);
@@ -83,14 +83,30 @@ $(document).ready(function() {
 	var r = 120;
 	var color = d3.scale.ordinal().range(selectedColors);
 	var canvas = d3.select("#donut-chart").append("svg").attr("width", 325).attr("height", 250);
+	var donutHover = d3.select("body").append("div") .attr("class", "tooltip");
 	var group = canvas.append("g").attr("transform", "translate(120,120)");
 	var arc = d3.svg.arc().innerRadius(50).outerRadius(r);
 	var pie = d3.layout.pie().value(function (d, i) { return data[i]; });
-	var arcs = group.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc");
+	var arcs = group.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc")
+		.on("mousemove",function(d,i){
+        	var mouseVal = d3.mouse(this);
+        	donutHover.style("display","none");
+        	donutHover
+        	.html( selectedFuels[i][0].toUpperCase() + selectedFuels[i].slice(1) + "</br>" + data[i])
+            .style("left", (d3.event.pageX+12) + "px")
+            .style("top", (d3.event.pageY-10) + "px")
+            .style("opacity", 1)
+            .style("display","block");
+        })
+        .on("mouseout",function(){donutHover.html(" ").style("display","none");})
+        .on("click",function(d){
+        	if(d3.select(this).attr("transform") == null){ d3.select(this).attr("transform","translate(42,0)"); }
+			else{ d3.select(this).attr("transform",null); }
+		});
 	arcs.append("path").attr("d", arc).attr("fill", function(d,i) {return selectedColors[i];});
 	arcs.append("text").attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";})
 		.attr("text-anchor", "middle").attr("font-size", "12px").style("fill", "white")
-		.text(function(d,i) {return data[i] / sum + "%";});
+		.text(function(d,i) {return Math.round(100*(data[i]/sum)) + "%";});
 
 	var legendG = canvas.selectAll(".legend").data(pie(data)).enter().append("g")
 				.attr("transform", function(d,i){ return "translate(" + (260) + "," + (i * 15 + 20) + ")"; })
