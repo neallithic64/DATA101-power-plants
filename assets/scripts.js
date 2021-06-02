c0824683f5c2c4fe36e59759963b8c2abdfac511
shannongailhoo <61385913+shannongailhoo@users.noreply.github.com>
2021-06-02 14:54:07 +0800

    Add files via upload
    Getting input from sidebar

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,17 +5,45 @@ $(document).ready(function() {
 		max: 2010,
 		values: [ 1961, 2010 ],
 		slide: function( event, ui ) {
-			console.log(ui.values);
+			var years = ui.values;
+			console.log(years);
+			$('#years').text(years);
 		}
 	});
 	
 	$("#capa-slider").slider({
 		range: true,
-		min: 1961,
-		max: 2010,
-		values: [ 1961, 2010 ],
+		min: 1,
+		max: 22000,
+		values: [ 1, 22000 ],
 		slide: function( event, ui ) {
-			console.log(ui.values);
+			var capacities = ui.values;
+			console.log(capacities);
+			$('#capacities').text(capacities);
 		}
 	});
+	
+	
+	var areas = new Array();
+	$('.area').click(function() {
+		if($(this).is(':checked'))
+        	areas.push($(this).attr("id"));
+        else {
+			var index = areas.indexOf($(this).attr("id"));
+			areas.splice(index,1);
+		}
+		console.log(areas);
+	});
+
+	var fuels = new Array();
+	$('.fuel').click(function() {
+		if($(this).is(':checked'))
+			fuels.push($(this).attr("id"));
+        else {
+			var index = fuels.indexOf($(this).attr("id"));
+			fuels.splice(index,1);
+		}
+		console.log(fuels);
+	});
+
 });
